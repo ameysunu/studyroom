ee47de9db09f632fd060ae42081181a81aac23e9
ameysunu <ameysunu00719@gmail.com>
2020-08-17 14:38:27 +0300

    register added

--- lib/signup.dart
+++ lib/signup.dart
@@ -22,6 +22,7 @@ class _SignUpState extends State<SignUp> {
         body: SingleChildScrollView(
           child: Container(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Padding(
                   padding: const EdgeInsets.fromLTRB(20.0, 0, 20, 0),
@@ -34,95 +35,131 @@ class _SignUpState extends State<SignUp> {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(20.0, 20, 20, 5),
-                  child: TextFormField(
-                    style:
-                        TextStyle(color: Colors.white, fontFamily: 'Poppins'),
-                    decoration: const InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                          width: 2.0,
+                  padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20.0, 20, 20, 5),
+                        child: TextFormField(
+                          style: TextStyle(
+                              color: Colors.white, fontFamily: 'Poppins'),
+                          decoration: const InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                                width: 2.0,
+                              ),
+                            ),
+                            icon: Icon(
+                              Icons.person,
+                              color: Colors.white,
+                            ),
+                            hintText: 'Ofcourse your name!',
+                            labelText: 'Username',
+                            labelStyle: TextStyle(
+                                color: Colors.white, fontFamily: 'Poppins'),
+                          ),
                         ),
                       ),
-                      icon: Icon(
-                        Icons.person,
-                        color: Colors.white,
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20.0, 10, 20, 5),
+                        child: TextFormField(
+                          style: TextStyle(
+                              color: Colors.white, fontFamily: 'Poppins'),
+                          decoration: const InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                                width: 2.0,
+                              ),
+                            ),
+                            icon: Icon(
+                              Icons.phone,
+                              color: Colors.white,
+                            ),
+                            hintText: 'Phone Number',
+                            labelText: 'Phone',
+                            labelStyle: TextStyle(
+                                color: Colors.white, fontFamily: 'Poppins'),
+                          ),
+                        ),
                       ),
-                      hintText: 'Ofcourse your name!',
-                      labelText: 'Username',
-                      labelStyle:
-                          TextStyle(color: Colors.white, fontFamily: 'Poppins'),
-                    ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20.0, 10, 20, 5),
+                        child: TextFormField(
+                          style: TextStyle(
+                              color: Colors.white, fontFamily: 'Poppins'),
+                          decoration: const InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                                width: 2.0,
+                              ),
+                            ),
+                            icon: Icon(
+                              Icons.email,
+                              color: Colors.white,
+                            ),
+                            hintText: 'Email',
+                            labelText: 'Email',
+                            labelStyle: TextStyle(
+                                color: Colors.white, fontFamily: 'Poppins'),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+                        child: TextFormField(
+                          obscureText: true,
+                          style: TextStyle(
+                              color: Colors.white, fontFamily: 'Poppins'),
+                          decoration: const InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                                width: 2.0,
+                              ),
+                            ),
+                            icon: Icon(
+                              Icons.lock,
+                              color: Colors.white,
+                            ),
+                            hintText: 'Password',
+                            labelText: 'Password',
+                            labelStyle: TextStyle(
+                                color: Colors.white, fontFamily: 'Poppins'),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: FlatButton(
+                          color: Colors.white,
+                          child: Text(
+                            "Register",
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 15,
+                            ),
+                          ),
+                          onPressed: () {
+                            null;
+                          },
+                        ),
+                      ),
+                    ],
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(20.0, 10, 20, 5),
-                  child: TextFormField(
-                    style:
-                        TextStyle(color: Colors.white, fontFamily: 'Poppins'),
-                    decoration: const InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
+                  padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Text(
+                      "Unable to create an account? Get in touch with us.",
+                      style: TextStyle(
                           color: Colors.white,
-                          width: 2.0,
-                        ),
-                      ),
-                      icon: Icon(
-                        Icons.phone,
-                        color: Colors.white,
-                      ),
-                      hintText: 'Phone Number',
-                      labelText: 'Phone',
-                      labelStyle:
-                          TextStyle(color: Colors.white, fontFamily: 'Poppins'),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20.0, 10, 20, 5),
-                  child: TextFormField(
-                    style:
-                        TextStyle(color: Colors.white, fontFamily: 'Poppins'),
-                    decoration: const InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                          width: 2.0,
-                        ),
-                      ),
-                      icon: Icon(
-                        Icons.email,
-                        color: Colors.white,
-                      ),
-                      hintText: 'Email',
-                      labelText: 'Email',
-                      labelStyle:
-                          TextStyle(color: Colors.white, fontFamily: 'Poppins'),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
-                  child: TextFormField(
-                    obscureText: true,
-                    style:
-                        TextStyle(color: Colors.white, fontFamily: 'Poppins'),
-                    decoration: const InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                          width: 2.0,
-                        ),
-                      ),
-                      icon: Icon(
-                        Icons.lock,
-                        color: Colors.white,
-                      ),
-                      hintText: 'Password',
-                      labelText: 'Password',
-                      labelStyle:
-                          TextStyle(color: Colors.white, fontFamily: 'Poppins'),
+                          fontSize: 13,
+                          fontFamily: 'Poppins'),
                     ),
                   ),
                 ),
