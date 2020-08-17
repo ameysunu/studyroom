b585441f5e7527fe115fd10418099bd26f751b24
ameysunu <ameysunu00719@gmail.com>
2020-08-17 13:53:08 +0300

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black54,
+          title: Text(
+            "StudyRoom",
+            style: TextStyle(
+              fontFamily: 'Poppins',
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: Text(
+                    "Online events that encourage immersive interactions and networking among students.",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Poppins',
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    height: 300,
+                    width: 300,
+                    child: Image.asset('images/main.png'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    style:
+                        TextStyle(color: Colors.white, fontFamily: 'Poppins'),
+                    decoration: const InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                          width: 2.0,
+                        ),
+                      ),
+                      icon: Icon(
+                        Icons.person,
+                        color: Colors.white,
+                      ),
+                      hintText: 'Email',
+                      labelText: 'Email',
+                      labelStyle:
+                          TextStyle(color: Colors.white, fontFamily: 'Poppins'),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20.0, 0, 20, 0),
+                  child: TextFormField(
+                    obscureText: true,
+                    style:
+                        TextStyle(color: Colors.white, fontFamily: 'Poppins'),
+                    decoration: const InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                          width: 2.0,
+                        ),
+                      ),
+                      icon: Icon(
+                        Icons.lock,
+                        color: Colors.white,
+                      ),
+                      hintText: 'Password',
+                      labelText: 'Password',
+                      labelStyle:
+                          TextStyle(color: Colors.white, fontFamily: 'Poppins'),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    color: Colors.white,
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                    onPressed: () {
+                      null;
+                    },
+                  ),
+                ),
+                InkWell(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child: Text(
+                      "Don't have an account? Register here!",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Poppins',
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  onTap: () {
+                    null;
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
